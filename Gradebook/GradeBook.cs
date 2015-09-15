6623e01776ba1f30c959a0906c2e6a39301c2b19
amynpeterson <amypeterson1982@gmail.com>
2015-09-14 22:12:26 -0400

    Assignment 4.10

---
+++ Gradebook/GradeBook.cs
@@ -0,0 +1,33 @@
+﻿// Assignment 4.10: GradeBook.cs
+// GradeBook class with a constructor to initialize the course name.
+
+using System;
+
+public class GradeBook
+{
+    // auto-implemented property CourseName implicity created an
+    // instance variable for this GradeBook's course name
+    public string CourseName { get; set; }
+
+    // auto-implemented property Instructor name implicity created an
+    // instance variable for this GradeBook's instructor 
+    public string InstructorName { get; set; }
+
+    // constructor initializes auto-implemented property
+    // CourseName with string supplied as argument
+    public GradeBook(string courseName, string instructorName)
+    {
+        CourseName = courseName ; // set CourseName to courseName
+        InstructorName = instructorName; // set InstructorName to instructorName
+    } // end constructor
+
+    // display a welcome message to the GradeBook user
+    public void DisplayMessage()
+    {
+        // use auto-implemented property CourseName to get the
+        // name of teh course that this GradeBook represents
+        Console.WriteLine("Welcome to the grade book for \n{0}!", CourseName);
+        // use auto-impleted propety InstructorName to get the name of the instructor
+        Console.WriteLine("This course is presented by: {0}", InstructorName);
+    } // end method DisplayMessage
+} // end Class GradeBook
---
+++ Gradebook/GradeBookTest.cs
@@ -0,0 +1,25 @@
+﻿// Assignemtn 4:10: GradeBookTest.cs
+// Create and manipulate a GradeBook object.
+using System;
+
+public class GradeBookTest
+{
+    // Main method begins program execution.
+    public static void Main(string[]args)
+    {
+       // create a GradeBook object and assign it to myGradeBook
+       GradeBook myGradeBook = new GradeBook("CS101 Introduction to C# Programming", "April Graves");
+
+       // display initial value of CourseName
+       Console.WriteLine("Initial course name is: '{0}'", myGradeBook.CourseName);
+
+       // prompt for and input course name
+       Console.WriteLine("Please enter the course name:");
+       myGradeBook.CourseName = Console.ReadLine(); // set CourseName
+       Console.WriteLine(); // output a blank line
+
+       // display welcome message after specifiying course name
+       myGradeBook.DisplayMessage();
+
+    } // end Main
+} // end class GradeBookTest
