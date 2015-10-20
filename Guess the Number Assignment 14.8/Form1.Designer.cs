5800a8ddf3228a8e1e75323f477d82a341fde3c1
amynpeterson <amypeterson1982@gmail.com>
2015-10-20 15:03:38 -0400

    Updating with Assignment 14.8 Guessing the Number WinForm

---
+++ Guess the Number Assignment 14.8/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Guess_the_Number_Assignment_14._8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(105, 198);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(149, 61);
+            this.btnGuess.TabIndex = 0;
+            this.btnGuess.Text = "Enter Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(105, 292);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(149, 61);
+            this.btnNewGame.TabIndex = 1;
+            this.btnNewGame.Text = "Start New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 60);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "I have a number between 1 and 1000. \r\nCan you guess my number?\r\nPlease enter your" +
+    " guess:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.BackColor = System.Drawing.Color.White;
+            this.txtGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuess.Location = new System.Drawing.Point(133, 129);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(100, 26);
+            this.txtGuess.TabIndex = 3;
+            this.txtGuess.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtGuess_KeyDown);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(130, 161);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 20);
+            this.lblResult.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGuess;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(326, 396);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnGuess);
+            this.Name = "Form1";
+            this.Text = "Guess the Number";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
